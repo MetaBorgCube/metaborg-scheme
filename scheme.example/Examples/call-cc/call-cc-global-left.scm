9095f918ae8ae7ae8f6061a4089e993430487643
Chiel Bruin <git@bruin.ch>
2019-04-24 22:04:32 +0200

    Updated to Stacy (Though everything is broken now)

--- scheme.example/Examples/call-cc/call-cc-global-left.scm
+++ scheme.example/Examples/call-cc/call-cc-global-left.scm
@@ -1,9 +1,7 @@
-(define add-x #f)						; -
-(define x 6)							; -
-(+ (call-with-current-continuation (	;
+(define add-x #f)								; -
+(+ 2 (call-with-current-continuation (	;
 	lambda (cont) (set! add-x cont) 3)	;
-	) x									;
+	)							;
 )										; 9
-(add-x 10)								; 16
-(set! x 2)								; 6
+(add-x 10)								; 6
 (add-x 40)								; 42
--- scheme.example/Examples/lambda/globalInLambda.scm
+++ scheme.example/Examples/lambda/globalInLambda.scm
@@ -1,2 +1,7 @@
 (define x 1)				; -
-((lambda (y) (+ x y)) 2)	; 3
+(define func ((lambda (y) (+ x y)) 2))
+
+(func 2)	; 3
+
+(define a (lambda (x) (+ x 29)))
+((set! a 12) 13)	; 42
