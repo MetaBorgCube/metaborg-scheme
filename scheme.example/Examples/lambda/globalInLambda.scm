249b193bdf8cd07234da0dd7dc56280eead4b170
Chiel Bruin <git@bruin.ch>
2019-04-26 14:53:09 +0200

    Lambdas

--- scheme.example/Examples/lambda/globalInLambda.scm
+++ scheme.example/Examples/lambda/globalInLambda.scm
@@ -1,7 +1,7 @@
 (define x 1)				; -
-(define func ((lambda (y) (+ x y)) 2))
+(define func (lambda (y) (+ x y)))
 
 (func 2)	; 3
 
-(define a (lambda (x) (+ x 29)))
+(define a (lambda (y) (+ y 29)))
 ((set! a 12) 13)	; 42
