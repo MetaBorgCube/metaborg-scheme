041e2de05922ec8dbd16b7a8ed4590be67cf57de
Chiel Bruin <git@bruin.ch>
2019-09-26 12:45:35 +0200

    Updated to fvm 1.2.0

--- scheme.example/Examples/call-cc/call-cc-global-left.scm
+++ scheme.example/Examples/call-cc/call-cc-global-left.scm
@@ -1,7 +1,9 @@
-(define add-x #f)								; -
-(+ 2 (call-with-current-continuation (	;
+(define add-x #f)						; -
+(define x 6)							; -
+(+ x (call-with-current-continuation (	;
 	lambda (cont) (set! add-x cont) 3)	;
-	)							;
+	)									;
 )										; 9
-(add-x 10)								; 6
-(add-x 40)								; 42
+(add-x 10)								; 16
+(set! x 2)								; 6
+(add-x 36)								; 42 / 8
--- scheme/src/main/strategies/scheme/strategies/InteropRegisterer.java
+++ scheme/src/main/strategies/scheme/strategies/InteropRegisterer.java
@@ -1,10 +1,10 @@
 package scheme.strategies;
 
+import org.metaborg.lang.framevm_core.FrameVM;
 import org.strategoxt.lang.JavaInteropRegisterer;
-import org.strategoxt.lang.Strategy;
 
 public class InteropRegisterer extends JavaInteropRegisterer {
     public InteropRegisterer() {
-    	super(new Strategy[] {});
+    	super(FrameVM.framevmExtend());
     }
 }
